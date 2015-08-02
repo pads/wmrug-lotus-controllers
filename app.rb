ba76554d4913862907dbe0af22a5814bbe4a5749
Ben Paddock <pads@thisispads.me.uk>
2015-08-02 17:49:30 +0100

    Use a view & pass it exposed variables from the controller

--- app.rb
+++ app.rb
@@ -3,8 +3,11 @@ require_relative 'app/controllers/home'
 
 class App < Sinatra::Base
 
+  set :views, settings.root + '/app/views'
+
   get '/' do
     action = Home::Index.new
     action.call(params)
+    erb :'home/index' , locals: action.exposures
   end
 end
--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -3,18 +3,15 @@ require 'lotus-controller'
 module Home
   class Index
     include Lotus::Action
+    expose :message
 
     # Whitelist
     params do
       param :message
     end
 
-    def initialize
-      self.format = :html
-    end
-
     def call(params)
-      status 200, "Hello #{params[:message]}"
+      @message = params[:message]
     end
   end
 end
